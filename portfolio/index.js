fd3d732d1d404f6331ca5cb1d1ed942369b80ac6
cheshirelion <georgepro100@gmail.com>
2022-01-27 17:12:23 +0300

    feat: add close menu at click links

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,15 +1,31 @@
-const close = document.querySelector('.hamburger');
-function toggleMenu() {
-   close.classList.toggle('closeBurger');
-};
-
-close.addEventListener('click', toggleMenu);
-
 (function () {
     const burger = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.nav');
+    // const navLinks = document.querySelectorAll('.nav-link');
     
     burger.addEventListener('click', () => {
          navMenu.classList.toggle('open')
      });
+
+    function toggleMenu() {
+        burger.classList.toggle('close');
+    };
+    
+    burger.addEventListener('click', toggleMenu);
+
+    // function closeMenu() {
+    //     navMenu.classList.remove('open');
+    //     burger.classList.remove('close');
+    // };
+
+    // navLinks.forEach((el) => el.addEventListener('click', closeMenu));
+    
+    function closeMenu(event) {
+        if (event.target.classList.contains('nav-link')) {
+            navMenu.classList.remove('open');
+            burger.classList.remove('close');
+        }
+    };
+    
+    navMenu.addEventListener('click', closeMenu);
 }());
