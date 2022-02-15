adca9bd0203f70528479f2dcb462fa0212287c8a
cheshirelion <georgepro100@gmail.com>
2022-02-16 01:41:51 +0300

    feat: add f show  random quotes to click btn and load page

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,11 +1,26 @@
 const blrQuotes = 'json/belquotes.json';
 const rusQuotes = 'json/quotes.json';
+const btn = document.querySelector('.btn');
+const quote = document.querySelector('.text');
+const author = document.querySelector('.autor');
 
 async function getQuotes() {
     const quotes = rusQuotes;
     const res = await fetch(quotes);
     const data = await res.json();
-    console.log(data);
+    showQuotes(data);
 }
 
-getQuotes();
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+
+const showQuotes = (data) => {
+    quote.textContent = data[getRandomInt(0, 100)].text;
+    author.textContent = data[getRandomInt(0, 100)].author;
+}
+
+btn.addEventListener('click', getQuotes);
+document.addEventListener('DOMContentLoaded', getQuotes);
