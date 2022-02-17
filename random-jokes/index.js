1eb468e27051db31906f51ab318112809359b1a3
cheshirelion <georgepro100@gmail.com>
2022-02-17 03:33:54 +0300

    feat: add f change lang

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,23 +1,27 @@
-const blrQuotes = 'json/belquotes.json';
+const belQuotes = 'json/belquotes.json';
 const rusQuotes = 'json/quotes.json';
 const btn = document.querySelector('.btn');
 const quote = document.querySelector('.text');
 const author = document.querySelector('.autor');
 const audio = new Audio('assets/audio/laser.mp3');
 const lang = document.querySelectorAll('.lang');
+const rus = document.getElementById('rus');
 let quotes = rusQuotes;
 
-function changeActive() {
-    lang.forEach(curent => curent.classList.remove('active'));
-    this.classList.add('active');
+const changeLang = () => {
+    if (rus.classList.contains('active')) {
+        quotes = rusQuotes;
+    } else {
+        quotes = belQuotes;
+    }
 };
 
-lang.forEach(el => {el.addEventListener('click', changeActive)});
-
 async function getQuotes() {
+    changeLang();
     const res = await fetch(quotes);
     const data = await res.json();
     showQuotes(data);
+    console.log(data)
 }
 
 const getRandomInt = (min, max) => {
@@ -31,6 +35,12 @@ const showQuotes = (data) => {
     author.textContent = data[getRandomInt(0, 99)].author;
 }
 
+function changeActive() {
+    lang.forEach(curent => curent.classList.remove('active'));
+    this.classList.add('active');
+};
+
+lang.forEach(el => {el.addEventListener('click', changeActive)});
 btn.addEventListener('click', getQuotes);
 document.addEventListener('DOMContentLoaded', getQuotes);
 btn.addEventListener('click', () => audio.play());
